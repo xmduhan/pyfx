ea2d537b22a466f00b4b08e13def9b900d9aeaff
xmduhan <18906021980@189.cn>
2013-07-22 21:44:13 +0800

    i

--- scalping/1-建表.sql
+++ scalping/1-建表.sql
@@ -27,3 +27,35 @@ create table TB_HFMarketsLtd_EURCAD_M1_1
   F8     VARCHAR2(255)
 );
 create index tb_hfmarketsltd_eurcad_m1_1_i1 on tb_hfmarketsltd_eurcad_m1_1(n);
+
+
+
+drop table TB_HFMarketsLtd_EURCAD_M1_95929;
+create table TB_HF_EURCAD_M1_95929 
+(
+  Time   DATE,
+  N      NUMBER,
+  Ask    NUMBER,
+  Bid    NUMBER,
+  Volume NUMBER,
+  NewBar NUMBER,
+  Rsi    NUMBER,
+  F8     VARCHAR2(255)
+);
+create index TB_HF_EURCAD_M1_95929_i1 on TB_HF_EURCAD_M1_95929(n);
+
+
+
+drop table TB_HFMarketsLtd_EURCAD_M1_524850;
+create table TB_HF_EURCAD_M1_524850 
+(
+  Time   DATE,
+  N      NUMBER,
+  Ask    NUMBER,
+  Bid    NUMBER,
+  Volume NUMBER,
+  NewBar NUMBER,
+  Rsi    NUMBER,
+  F8     VARCHAR2(255)
+);
+create index TB_HF_EURCAD_M1_524850_i1 on TB_HF_EURCAD_M1_524850(n);
--- scalping/2-分析.sql
+++ scalping/2-分析.sql
@@ -1,10 +1,19 @@
 cl;
 set echo on;
 
+drop table duh0722_1;
+create table duh0722_1 as 
+--select * from tb_hfmarketsltd_eurcad_m1 where to_char(time,'yyyymmdd') >= '20130619'
+--select * from TB_HF_EURCAD_M1_95929
+select * from TB_HF_EURCAD_M1_524850
+;
+create index duh0722_1_i1 on duh0722_1(n);
+
+
 drop table fxtmp001;
 create table fxtmp001 as 
 select *
-  from tb_hfmarketsltd_eurcad_m1
+  from duh0722_1
  where 1=1
    and to_char(time, 'hh24') in ('23', '00', '01', '02')
    and rsi < 30;
@@ -18,14 +27,14 @@ drop table fxtmp002;
 create table fxtmp002 as 
 select a.*,
        (select min(n)
-          from tb_hfmarketsltd_eurcad_m1 b
+          from duh0722_1 b
          where b.n > a.n
-           and b.n < a.n + 1200
+           and b.n < a.n + 240
            and b.bid - a.ask >= 0.0005) n1,
        (select min(n)
-          from tb_hfmarketsltd_eurcad_m1 b
+          from duh0722_1 b
          where b.n > a.n
-           and b.n < a.n + 1200
+           and b.n < a.n + 240
            and b.bid - a.ask <= -0.0015) n2
   from fxtmp001 a;
   
@@ -34,7 +43,7 @@ select count(*) from fxtmp002;
 alter table fxtmp002 add(nx number);
 update fxtmp002 set nx = n1 where n1 is not null and n2 is null;
 update fxtmp002 set nx = n2 where n1 is null and n2 is not null;
-update fxtmp002 set nx = n + 1200 where n1 is null and n2 is null;
+update fxtmp002 set nx = n + 240 where n1 is null and n2 is null;
 update fxtmp002 set nx = n1  where n1 is not null and n2 is not null and n1<n2;
 update fxtmp002 set nx = n2  where n1 is not null and n2 is not null and n1>n2;
 commit;
@@ -42,7 +51,7 @@ commit;
 drop table fxtmp003;
 create table fxtmp003 as 
 select a.*, b.bid - a.ask profit,to_char(a.time,'yyyymmdd-hh24') t1,to_char(a.time,'hh24') hh
-  from fxtmp002 a, tb_hfmarketsltd_eurcad_m1 b
+  from fxtmp002 a, duh0722_1 b
  where a.nx = b.n;
 delete from fxtmp003 a where n <> (select min(n) from fxtmp003 b where b.t1 = a.t1);
 commit;
